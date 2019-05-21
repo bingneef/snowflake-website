acf2a19d6c42978ae686a7f79585795cacd5a690
Bing Steup <bingsteup@gmail.com>
2019-05-21 16:01:51 +0200

    Add visual tests for portfolio

--- cypress/integration/pages/homepage_spec.js
+++ cypress/integration/pages/homepage_spec.js
@@ -7,10 +7,15 @@ describe("The Home Page", function() {
 
   it("scrolls to the next section on CTA", function() {
     cy.visit("http://localhost:8010");
+
+    // CTA is in viewport
+    cy.isInViewport("[test-id='cta']");
     cy.get("[test-id='cta']").click();
 
+    // Wait for scroll
     cy.wait(1500);
 
+    // Scrolled to #portfolio
     cy.isAtTopOfViewport("#portfolio");
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,6 +1,20 @@
+Cypress.Commands.add("isInViewport", element => {
+  cy.get(element).then($el => {
+    const height = Cypress.$(cy.state("window")).height();
+    const width = Cypress.$(cy.state("window")).width();
+    const rect = $el[0].getBoundingClientRect();
+
+    expect(rect.top).to.be.greaterThan(0);
+    expect(rect.bottom).to.be.lessThan(height);
+    expect(rect.left).to.be.greaterThan(0);
+    expect(rect.right).to.be.lessThan(width);
+  });
+});
+
 Cypress.Commands.add("isAtTopOfViewport", element => {
   cy.get(element).then($el => {
     const rect = $el[0].getBoundingClientRect();
+
     expect(rect.top).to.equal(0);
   });
 });
